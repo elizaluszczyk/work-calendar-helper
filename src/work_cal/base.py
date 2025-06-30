9178eb63e3065f28475e12c0ff02bb85e98bdf0c
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-06-30 16:57:38 +0200

    feat: add default config values

---
+++ src/work_cal/base.py
@@ -0,0 +1,7 @@
+import pathlib
+
+CONFIG_PATH = pathlib.Path.home() / ".config" / "cal_manager" / "config.toml"
+DEFAULT_SHIT_START_HOUR: str = "10:00"
+DEFAULT_SHIFT_END_HOUR: str = "21:00"
+DEFAULT_SHIFT_DURATION: int = 10
+DEFAULT_WORKER_NAME: str = "Worker"
