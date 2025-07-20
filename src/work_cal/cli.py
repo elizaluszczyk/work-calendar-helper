1adaee94ee099510f96e368cd8e5076e9be08bfd
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-07-20 16:41:56 +0200

    feat: add calendar file dumping as a new entrypoint in cli

--- src/work_cal/cli.py
+++ src/work_cal/cli.py
@@ -1,8 +1,13 @@
 import calendar
 from datetime import date, datetime
+from pathlib import Path
 
 import click
+from pyfzf.pyfzf import FzfPrompt
 
+from work_cal.base import DEFAULT_MONTH_DUMP_LOCATION
+from work_cal.calendar.shift_parsing import shift_state_dump_to_calendar
+from work_cal.models import ShiftStateDump
 from work_cal.tui.shift_planner import ShiftPlannerApp
 
 
@@ -27,6 +32,40 @@ def planner(month: int, year: int) -> None:
     app.sub_title = "Shift Planner"
     app.run()
     app.planner_state.dump_shift_state()
+
+
+@main.command()
+@click.argument("filename", type=str)
+def dump(filename: str) -> None:
+    available_dump_files: list[Path] = []
+    for path in DEFAULT_MONTH_DUMP_LOCATION.iterdir():
+        if not path.is_file():
+            continue
+
+        available_dump_files.append(path)
+
+    fzf = FzfPrompt()
+
+    selected_dump_file_list: list[str] = fzf.prompt(available_dump_files, fzf_options="--height=~40%")
+
+    if len(selected_dump_file_list) != 1:
+        return
+
+    [selected_dump_file] = selected_dump_file_list
+
+    selected_dump_file = Path(selected_dump_file)
+
+    if selected_dump_file not in available_dump_files:
+        msg = "Something went wrong"
+        raise RuntimeError(msg)
+
+    json_dump = selected_dump_file.read_text("utf-8")
+
+    shift_state_dump: ShiftStateDump = ShiftStateDump.model_validate_json(json_dump)
+
+    calendar_obj = shift_state_dump_to_calendar(shift_state_dump)
+
+    Path(filename).write_text(calendar_obj.serialize(), encoding="utf-8")
 
 
 if __name__ == "__main__":
